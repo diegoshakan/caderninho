6eff4a79f0999d759436591e54594c6e50362b13
Diego Araujo <diego_shakan@hotmail.com>
2022-06-27 10:46:21 -0300

    feat(payments): rearrange resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user_1 = User.create(email: "diego@example.com",
+                     first_name: FFaker::NameBR.first_name,
+                     last_name: FFaker::NameBR.last_name,
+                     password: "changeme",
+                     password_confirmation: "changeme")
+
+Payment.create(title: "Energia", description: "Sem gato", value: 200.00, date: Date.today, user: user_1)
